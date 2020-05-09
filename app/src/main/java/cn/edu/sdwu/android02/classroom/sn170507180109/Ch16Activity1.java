b5559c134c3326210fd830a113c9d62626a6530b
zhangluxian <352455618@qq.com>
2020-05-09 12:52:20 +0800

    2020/05/09

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180109/Ch16Activity1.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180109/Ch16Activity1.java
@@ -4,13 +4,18 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.media.Image;
+import android.media.ImageReader;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -20,7 +25,11 @@ import android.util.Log;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class Ch16Activity1 extends AppCompatActivity {
@@ -31,6 +40,36 @@ public class Ch16Activity1 extends AppCompatActivity {
     private CaptureRequest.Builder captureRequestBuilder;//请求的构造器
     private CaptureRequest previewRequest;
     private CameraCaptureSession cameraCaptureSession;
+    private ImageReader imageReader;//接收相机生成的静态图像
+
+    public  void  takephoto(View view){
+        //点击快门，生成静态图像
+        if(cameraDevice!=null) {
+            //使用Builder来创建请求
+            try {
+                CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+                builder.addTarget(imageReader.getSurface());
+                //停止连续取景
+                cameraCaptureSession.stopRepeating();
+                //捕捉静态图像
+                cameraCaptureSession.capture(builder.build(),new CameraCaptureSession.CaptureCallback(){
+                    @Override
+                    public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                        //捕获完成后，回复连续取景
+                        try{
+                            session.setRepeatingRequest(previewRequest,null,null);
+                        }catch(Exception e){
+                            Log.e(Ch16Activity1.class.toString(),e.toString());
+                        }
+                    }
+                },null);
+            }catch (Exception e){
+                Log.e(Ch16Activity1.class.toString(),e.toString());
+            }
+
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,10 +95,40 @@ public class Ch16Activity1 extends AppCompatActivity {
                     //创建一个捕捉请求CaptureRequest
                     captureRequestBuilder=cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
                     captureRequestBuilder.addTarget(surface);//指定视频输出的位置
+
+                    imageReader=ImageReader.newInstance(1024,768, ImageFormat.JPEG,2);
+                    imageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                        @Override
+                        public void onImageAvailable(ImageReader imageReader) {
+                            //当照片数据可用时，激发该方法
+                            //获取捕获的照片数据
+                            Image image=imageReader.acquireNextImage();
+                            ByteBuffer buffer=image.getPlanes()[0].getBuffer();
+                            byte[] bytes=new byte[buffer.remaining()];
+                            buffer.get(bytes);
+                            //写文件
+                            File file=new File(Environment.getExternalStorageDirectory(),"abcd.jpg");
+                            FileOutputStream outputStream=null;
+                            try{
+                                outputStream=new FileOutputStream(file);
+                                outputStream.write(bytes);
+                                Toast.makeText(Ch16Activity1.this,"save:"+file,Toast.LENGTH_LONG).show();
+                            }catch(Exception e){
+                                Log.e(Ch16Activity1.class.toString(),e.toString());
+                            }finally {
+                                try{
+                                    outputStream.flush();
+                                    outputStream.close();
+                                }catch (Exception ee){
+                                    Log.e(Ch16Activity1.class.toString(),ee.toString());
+                                }
+                            }
+                        }
+                    },null);
                     //创建一个相机捕捉会话
                     // 参数1代表后续预览或拍照使用的组件
                     //参数2代表的是监听器，创建会话完成后执行的方法
-                    cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
+                    cameraDevice.createCaptureSession(Arrays.asList(surface,imageReader.getSurface()), new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                             //会话创建完成后，我们可以在参数中得到会话的对象
